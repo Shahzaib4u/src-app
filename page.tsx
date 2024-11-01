a72988609c93d513b03c68ec18825dbcd96d8a99
Shahzaib Pervaiz <shahzaib.89@hotmail.com>
2024-11-01 17:44:09 +0500

    Alto
    export default function Alto () {
        return (
            <div>
    
                <div className="flex items-center justify-center text-3xl font-bold my-12">
                    <h1><u>Suzuki Alto 2024 Price In Pakistan, Images, Reviews & Specs</u></h1>
                </div>
    
                <div className="flex items-center justify-center w-full my-12">
                    <img src="/Alto Details.jfif" alt="Suzuki Alto" />
                </div>
    
                <div className="flex items-center justify-center w-full text-2xl">
                    <h1>Vehicle Description</h1>
                </div>
    
                <div>
    
                    <div className="flex items-center justify-center w-full gap-4 mt-8">
                        <p><b>Number Of Doors:</b> 4</p>
                        <p><b>Engine:</b> 1000 CC</p>
                        <p><b>Condition:</b> 9/10</p>
                        <p><b>Driven:</b> 9,500 Km</p>
                        <p><b>Suspension Type:</b> Soft Suspension</p>
                    </div>
    
                    <div className="flex items-center justify-center w-full gap-4 my-6">
                        <p><b>Avg:</b> 20 - 22 Km per ltr</p>
                        <p><b>Transmission:</b> Automatic</p>
                        <p><b>Fuel Type:</b> Petrol</p>
                    </div>
                    
                </div>
    
                <div className="flex items-center justify-center w-full ">
    
                    <ul>
                        <li><h1  className="text-2xl text-green-600">PKR 30,45,000</h1></li>
                        <li>
                            <a href="/Checkout">
                                <button className="bg-blue-700 py-2 px-2 my-8 mx-4 text-white text-lg rounded-lg">Make Payment</button>
                            </a>
                            </li>
                    </ul>
    
                </div>
    
            </div>
        )
    }

---
+++ page.tsx
@@ -0,0 +1,51 @@
+
+export default function Alto () {
+    return (
+        <div>
+
+            <div className="flex items-center justify-center text-3xl font-bold my-12">
+                <h1><u>Suzuki Alto 2024 Price In Pakistan, Images, Reviews & Specs</u></h1>
+            </div>
+
+            <div className="flex items-center justify-center w-full my-12">
+                <img src="/Alto Details.jfif" alt="Suzuki Alto" />
+            </div>
+
+            <div className="flex items-center justify-center w-full text-2xl">
+                <h1>Vehicle Description</h1>
+            </div>
+
+            <div>
+
+                <div className="flex items-center justify-center w-full gap-4 mt-8">
+                    <p><b>Number Of Doors:</b> 4</p>
+                    <p><b>Engine:</b> 1000 CC</p>
+                    <p><b>Condition:</b> 9/10</p>
+                    <p><b>Driven:</b> 9,500 Km</p>
+                    <p><b>Suspension Type:</b> Soft Suspension</p>
+                </div>
+
+                <div className="flex items-center justify-center w-full gap-4 my-6">
+                    <p><b>Avg:</b> 20 - 22 Km per ltr</p>
+                    <p><b>Transmission:</b> Automatic</p>
+                    <p><b>Fuel Type:</b> Petrol</p>
+                </div>
+                
+            </div>
+
+            <div className="flex items-center justify-center w-full ">
+
+                <ul>
+                    <li><h1  className="text-2xl text-green-600">PKR 30,45,000</h1></li>
+                    <li>
+                        <a href="/Checkout">
+                            <button className="bg-blue-700 py-2 px-2 my-8 mx-4 text-white text-lg rounded-lg">Make Payment</button>
+                        </a>
+                        </li>
+                </ul>
+
+            </div>
+
+        </div>
+    )
+}
